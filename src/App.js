11a8917ce7a394c0927554f8b478666e5aa4b713
Marco Parrone <marco@marcoparrone.com>
2021-01-07 18:58:07 +0100

    Changed license terms.

--- src/App.js
+++ src/App.js
@@ -1077,14 +1077,7 @@ class NotesList extends React.Component {
                 <h2 className="mdc-dialog__title" id="about-dialog-title">About</h2>
                 <div className="mdc-dialog__content" id="about-dialog-content">
                   <p>Copyright &copy; 2017,2019,2020,2021 Marco Parrone.</p>
-                  <p>Permission is hereby granted, free of charge, to any person obtaining a copy
-                  of this software and associated documentation files (the "Software"), to deal
-                  in the Software without restriction, including without limitation the rights
-                  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                  copies of the Software, and to permit persons to whom the Software is
-                          furnished to do so, subject to the following conditions:</p>
-                  <p>The above copyright notice and this permission notice shall be included in all
-                          copies or substantial portions of the Software.</p>
+                  <p>All Rights Reserved.</p>
                   <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
                   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
